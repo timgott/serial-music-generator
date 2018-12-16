ed50094316319bcde8caec533bfbedb182e2bbe1
Tim <timg00@outlook.de>
2018-12-16 14:14:42 +0100

    Added row melody composition and note repeating

--- src/main.js
+++ src/main.js
@@ -15,16 +15,21 @@ function shuffle(a) {
     return a;
 }
 
-function clamp12(x) {
-    while (x < 0)
-        x += 12;
-    while (x >= 12)
-        x -= 12;
-    
-    return x;
+function chance(p) {
+    return Math.random() < p;
 }
 
-function generate() {
+// melody calculation
+function generateMatrix() {
+    function clamp12(x) {
+        while (x < 0)
+            x += 12;
+        while (x >= 12)
+            x -= 12;
+        
+        return x;
+    }
+
     let baseRow = shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]);
     let baseNumber = baseRow[0];
 
@@ -41,24 +46,6 @@ function generate() {
     }
 
     return matrix;
-}
-
-function toneRowToStave(system, row) {
-    let notes = [];
-    for (let i = 0; i < row.length; i++) {
-        notes[i] = vf.StaveNote({
-            keys: [Vex.Flow.integerToNote(row[i])+"/4"],
-            duration: "4"
-        })
-    }
-
-    let voice = vf.Voice({time: "12/4"}).addTickables(notes);
-
-    system.addStave({
-        voices: [
-            voice
-        ]
-    }).addClef("treble").addTimeSignature("4/4")
 }
 
 function krebs(reihe) {
@@ -82,13 +69,82 @@ function getUmkehrung(matrix, n = 0) {
     return matrix[n];
 }
 
-let matrix = generate();
+function getRandomRow(matrix) {
+    const n = Math.floor(Math.random() * 12);
+    const row = chance(0.5) ? getReihe(matrix, n) : getUmkehrung(matrix, n);
+    return chance(0.5) ? row : krebs(row);
+}
 
-let score = vf.EasyScore();
+function repeatNotesPass(sourceRow) {
+    function addNotes(row, i) {
+        if (i >= sourceRow.length)
+            return row;
+
+        const newRow = row.concat(sourceRow[i]);
+        // double note
+        if (chance(0.1))
+            return addNotes(newRow, i)
+        else
+            return addNotes(newRow, i+1)
+    }
+
+    return addNotes([], 0);
+}
+
+function addRows(minLength, original) {
+    const newRow = repeatNotesPass(getRandomRow(matrix));
+    const combined = original.concat(newNotes)
+
+    if (combined.length < minLength)
+        return addRows(minLength, combined);
+    else
+        return combined;
+}
+
+const rhythmicUnit = 16;
+
+function compose(minLength) {
+    const rhythmicPatterns = [[2, 2, 2, 4], [4, 1]];
+    const matrix = generateMatrix();
+
+    addRows(minLength, getReihe(matrix));
+}
+
+
+function toneRowToStave(system, row) {
+    let notes = [];
+    for (let i = 0; i < row.length; i++) {
+        // read key from row integer
+        let key = Vex.Flow.integerToNote(row[i]);
+        let note = vf.StaveNote({
+            keys: [key + "/4"],
+            duration: "4"
+        });
+
+        // add accidentals
+        let accidental = note.keyProps[0].accidental
+        
+        if (accidental)
+            note.addAccidental(0, vf.Accidental({ type: accidental }));
+
+        notes[i] = note
+    }
+
+    let voice = vf.Voice({time: "12/4"}).addTickables(notes);
+
+    system.addStave({
+        voices: [
+            voice
+        ]
+    }).addClef("treble").addTimeSignature("4/4")
+}
+
+let matrix = generateMatrix();
+
 let system = vf.System();
 
 toneRowToStave(system, getReihe(matrix));
-toneRowToStave(system, getUmkehrung(matrix, 1));
-toneRowToStave(system, getReihe(matrix, 2));
+toneRowToStave(system, getUmkehrung(matrix));
+toneRowToStave(system, getReihe(matrix, 1));
 
 vf.draw();
